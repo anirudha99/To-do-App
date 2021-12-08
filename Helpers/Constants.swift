c7623f0e1ecef67c30be63d77e5f98119b1b07f4
anirudha mayya <47984807+anirudha99@users.noreply.github.com>
2021-12-08 09:11:55 +0530

    Add files via upload

---
+++ Helpers/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  LoginPageLayout
+//
+//  Created by Anirudha SM on 19/10/21.
+//
+
+import Foundation
+import UIKit
+
+struct Constants{
+    
+    struct Storyboard{
+        static let homeViewController = "HomeVC"
+    }
+    
+    struct Image{
+        static let profileDefaultImage: UIImage = UIImage(systemName: "person")!
+        static let archive = UIImage(systemName: "folder.fill.badge.plus")
+        static let unarchive = UIImage(systemName: "folder.fill.badge.minus")
+        static let remind = UIImage(systemName: "alarm.fill")
+        static let delete = UIImage(systemName:"trash")
+    }
+    
+}
